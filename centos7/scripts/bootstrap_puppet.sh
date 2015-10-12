f57747087d4e1f7399af99253a8768afd6a882bd
Josh Smith <juicewvu@gmail.com>
2015-10-12 11:54:00 -0400

    Updated a cople of latent refrences to centos6 in the centos7 module.

--- centos7/scripts/bootstrap_puppet.sh
+++ centos7/scripts/bootstrap_puppet.sh
@@ -3,6 +3,6 @@
 
 #add the puppet repository
 echo "Installing puppet agent."
-yum install https://yum.puppetlabs.com/puppetlabs-release-el-6.noarch.rpm -y
+yum install https://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm -y
 yum install puppet -y
 
